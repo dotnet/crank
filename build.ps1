9a89038806bdee51505156f1a27cf2e0fc729290
Sébastien Ros <sebastienros@gmail.com>
2020-05-08 13:52:00 -0700

    Onboarding arcade (#19)

--- build.ps1
+++
@@ -1,4 +0,0 @@
-New-Item -ItemType Directory -Force -Path .\.build
-wget "https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1" -OutFile ".\.build\dotnet-install.ps1"
-.\.build\dotnet-install.ps1 -Channel 3.1.302 -SkipNonVersionedFiles -Version 32.1.302
-dotnet build -c Release
