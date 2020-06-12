eb5dbd1dd5067dac55283121351ac2770ec0ce1f
Sébastien Ros <sebastienros@gmail.com>
2020-06-12 16:00:23 -0700

    Adding license information (#39)

--- src/Microsoft.Crank.Controller/CsvResult.cs
+++ src/Microsoft.Crank.Controller/CsvResult.cs
@@ -1,4 +1,8 @@
-﻿using CsvHelper.Configuration;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CsvHelper.Configuration;
 
 namespace Microsoft.Crank.Controller
 {
