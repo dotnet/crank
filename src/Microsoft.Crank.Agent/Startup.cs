1dc1e948e1b9e507337f94f3e91cc64b8179a240
Sebastien Ros <sebastienros@gmail.com>
2020-09-30 13:03:33 -0700

    Fixing process id race

--- src/Microsoft.Crank.Agent/Startup.cs
+++ src/Microsoft.Crank.Agent/Startup.cs
@@ -635,9 +635,7 @@ namespace Microsoft.Crank.Agent
                                             {
                                                 process = await StartProcess(hostname, Path.Combine(tempDir, benchmarksDir), job, _dotnethome);
 
-                                                job.ProcessId = process.Id;
-
-                                                Log.WriteLine($"Process started: {process.Id}");
+                                                Log.WriteLine($"Process started: {job.ProcessId}");
 
                                                 workingDirectory = process.StartInfo.WorkingDirectory;
                                             }
@@ -3552,6 +3550,8 @@ namespace Microsoft.Crank.Agent
             stopwatch.Start();
             process.Start();
 
+            job.ProcessId = process.Id;
+            
             process.BeginOutputReadLine();
             process.BeginErrorReadLine();
 
