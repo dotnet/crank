31c5a5b41fca957c3071765363068442a3e242ca
Sébastien Ros <sebastienros@gmail.com>
2020-10-23 17:04:36 -0700

    Adding AzureDevOpsWorker (#150)

---
+++ src/Microsoft.Crank.AzureDevOpsWorker/JobPayload.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AzureDevOpsWorker
+{
+
+    public class JobPayload
+    {
+        private static TimeSpan DefaultJobTimeout = TimeSpan.FromMinutes(10);
+
+        [JsonConverter(typeof(TimeSpanConverter))]
+        public TimeSpan Timeout { get; set; } = DefaultJobTimeout;
+
+        public string Name { get; set; }
+        public string[] Args { get; set; }
+
+        public string RawPayload { get; set; }
+
+        public static JobPayload Deserialize(byte[] data)
+        {
+            try
+            {
+                var str = Encoding.UTF8.GetString(data);
+
+                // Azure Devops adds a DataContractSerializer preamble to the message, and also
+                // an invalid JSON char at the end of the message
+                str = str.Substring(str.IndexOf("{"));
+                str = str.Substring(0, str.LastIndexOf("}") + 1);
+                var result = System.Text.Json.JsonSerializer.Deserialize<JobPayload>(str, new System.Text.Json.JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                result.RawPayload = str;
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error while parsing message body: " + Encoding.UTF8.GetString(data), e);
+            }
+        }
+    }
+}
---
+++ src/Microsoft.Crank.AzureDevOpsWorker/ProcessResult.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace AzureDevOpsWorker
+{
+    public class ProcessResult
+    {
+        public ProcessResult(int exitCode, string output, string error)
+        {
+            ExitCode = exitCode;
+            Output = output;
+            Error = error;
+        }
+
+        public string Error { get; }
+        public int ExitCode { get; }
+        public string Output { get; }
+    }
+}
