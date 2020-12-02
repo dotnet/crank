4c8a7aa306a55c117bf0ead1bfdab26c84d68071
Sebastien Ros <sebastienros@gmail.com>
2020-12-02 13:31:26 -0800

    Displaying collection progress

--- src/Microsoft.Crank.Controller/JobConnection.cs
+++ src/Microsoft.Crank.Controller/JobConnection.cs
@@ -634,7 +634,7 @@ namespace Microsoft.Crank.Controller
                     traceDestination = traceDestination + "." + DateTime.Now.ToString("MM-dd-HH-mm-ss") + traceExtension;
                 }
 
-                Log.Write($"Collecting trace file '{traceDestination}' ...");
+                Log.Write($"Collecting trace file, this can take several seconds");
 
                 var uri = _serverJobUri + "/trace";
                 var response = await _httpClient.PostAsync(uri, new StringContent(""));
@@ -646,7 +646,7 @@ namespace Microsoft.Crank.Controller
 
                     if (state == JobState.TraceCollecting)
                     {
-                        // Server is collecting the trace
+                        Console.Write(".");
                     }
                     else
                     {
@@ -656,7 +656,9 @@ namespace Microsoft.Crank.Controller
                     await Task.Delay(1000);
                 }
 
-                Log.Write($"Downloading trace file...");
+                Console.WriteLine();
+
+                Log.Write($"Downloading trace file {traceDestination} ...");
 
                 try
                 {
