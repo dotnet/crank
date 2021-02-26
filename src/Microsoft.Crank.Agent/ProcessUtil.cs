426936bcba14e58b0ce907ba05505ad630c07d61
Mike Harder <mharder@microsoft.com>
2021-02-26 14:53:29 -0800

    [ProcessUtil] Ensure output buffers are flushed before exit (#245)

--- src/Microsoft.Crank.Agent/ProcessUtil.cs
+++ src/Microsoft.Crank.Agent/ProcessUtil.cs
@@ -113,6 +113,10 @@ namespace Microsoft.Crank.Agent
 
             process.Exited += (_, e) =>
             {
+                // Even though the Exited event has been raised, WaitForExit() must still be called to ensure the output buffers
+                // have been flushed before the process is considered completely done.
+                process.WaitForExit();
+
                 if (throwOnError && process.ExitCode != 0)
                 {
                     processLifetimeTask.TrySetException(new InvalidOperationException($"Command {filename} {arguments} returned exit code {process.ExitCode}"));
