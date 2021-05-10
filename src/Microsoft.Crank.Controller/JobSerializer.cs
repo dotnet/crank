ea81a866146a5733eba9aac74c79afd66cb8efd6
Sebastien Ros <sebastienros@gmail.com>
2021-05-10 09:59:07 -0700

    Fix namespace

--- src/Microsoft.Crank.Controller/JobSerializer.cs
+++ src/Microsoft.Crank.Controller/JobSerializer.cs
@@ -3,7 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Threading.Tasks;
 using Microsoft.Crank.Models;
 using Newtonsoft.Json;
