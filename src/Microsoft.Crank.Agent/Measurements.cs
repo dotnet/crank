dc5f9826e5624c3a37d765329ba4e9725b8aa5c5
Sébastien Ros <sebastienros@gmail.com>
2022-12-16 16:11:40 -0800

    Reuse string constants (#518)

---
+++ src/Microsoft.Crank.Agent/Measurements.cs
@@ -0,0 +1,21 @@
+﻿namespace Microsoft.Crank.Agent
+{
+    public class Measurements
+    {
+        public const string BenchmarksCpu = "benchmarks/cpu";
+        public const string BenchmarksCpuRaw = "benchmarks/cpu/raw";
+        public const string BenchmarksWorkingSet = "benchmarks/working-set";
+        public const string BenchmarksPrivateMemory = "benchmarks/private-memory";
+        public const string BenchmarksBuildTime = "benchmarks/build-time";
+        public const string BenchmarksStartTime = "benchmarks/start-time";
+        public const string BenchmarksPublishedSize = "benchmarks/published-size";
+        public const string BenchmarksSymbolsSize = "benchmarks/symbols-size";
+        public const string BenchmarksMemorySwap = "benchmarks/memory/swap";
+        public const string BenchmarksCpuPeriodsTotal = "benchmarks/cpu/periods/total";
+        public const string BenchmarksCpuPeriodsThrottled = "benchmarks/cpu/periods/throttled";
+        public const string BenchmarksCpuThrottled = "benchmarks/cpu/throttled";
+        public const string BenchmarksNetSdkVersion = "netSdkVersion";
+        public const string BenchmarksAspNetCoreVersion = "AspNetCoreVersion";
+        public const string BenchmarksNetCoreAppVersion = "NetCoreAppVersion";
+    }
+}
