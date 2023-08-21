99ac5646874aee8d96277da8e43599cc6174dd30
Parker Bibus <parkerbibus@microsoft.com>
2023-08-21 15:03:54 -0700

    Update WrkProcess.cs with using System.Runtime.InteropServices;
    Add System.Runtime.InteropServices to file to find architecture information.

--- src/Microsoft.Crank.Jobs.Wrk/WrkProcess.cs
+++ src/Microsoft.Crank.Jobs.Wrk/WrkProcess.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
