d2dda1ea3874e718ba593c6e0b8c6b9e3c2c904e
Sébastien Ros <sebastienros@gmail.com>
2023-10-18 18:01:52 -0700

    Log group information for cpu sets (#645)

--- src/Microsoft.Crank.Agent/WindowsLimiter.cs
+++ src/Microsoft.Crank.Agent/WindowsLimiter.cs
@@ -92,6 +92,11 @@ namespace Microsoft.Crank.Agent
 
                 var result = SetProcessDefaultCpuSets(_safeProcess, cpuSets, (uint)cpuSets.Length);
                 Log.Info($"Limiting CpuSet ids: {String.Join(',', cpuSets)}, {result} ({_job.Service}:{_job.Id})");
+
+                foreach (var csi in ssi)
+                {
+                    Log.Info($"Id: {csi.CpuSet.Id}; NumaNodeIndex: {csi.CpuSet.NumaNodeIndex}; LogicalProcessorIndex: {csi.CpuSet.LogicalProcessorIndex}; CoreIndex: {csi.CpuSet.CoreIndex}; Group: {csi.CpuSet.Group}");
+                }
             }
 
             if (_hasJobObj)
