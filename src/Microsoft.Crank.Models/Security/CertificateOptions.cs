8c67ccda79c7269c8b6acc8f5258e3c90eca1443
Sebastien Ros <sebastienros@gmail.com>
2024-06-03 16:17:05 -0700

    Load certificate options from ENV if available

--- src/Microsoft.Crank.Models/Security/CertificateOptions.cs
+++ src/Microsoft.Crank.Models/Security/CertificateOptions.cs
@@ -1,4 +1,6 @@
-﻿namespace Microsoft.Crank.Models.Security
+﻿using System;
+
+namespace Microsoft.Crank.Models.Security
 {
     public class CertificateOptions
     {
@@ -9,6 +11,26 @@
 
         public CertificateOptions(string clientId, string tenantId, string thumbprint, string path)
         {
+            if (!string.IsNullOrEmpty(clientId) && !string.IsNullOrEmpty(Environment.GetEnvironmentVariable(clientId)))
+            {
+                clientId = Environment.GetEnvironmentVariable(clientId);
+            }
+
+            if (!string.IsNullOrEmpty(tenantId) && !string.IsNullOrEmpty(Environment.GetEnvironmentVariable(tenantId)))
+            {
+                tenantId = Environment.GetEnvironmentVariable(tenantId);
+            }
+
+            if (!string.IsNullOrEmpty(thumbprint) && !string.IsNullOrEmpty(Environment.GetEnvironmentVariable(thumbprint)))
+            {
+                thumbprint = Environment.GetEnvironmentVariable(thumbprint);
+            }
+
+            if (!string.IsNullOrEmpty(path) && !string.IsNullOrEmpty(Environment.GetEnvironmentVariable(path)))
+            {
+                path = Environment.GetEnvironmentVariable(path);
+            }
+
             ClientId = clientId;
             TenantId = tenantId;
             Thumbprint = thumbprint;
