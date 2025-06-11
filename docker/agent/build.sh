5b38e755c2683d4d48a646b8fb75432b1e841bc0
Parker Bibus <parkerbibus@microsoft.com>
2025-06-11 10:37:45 -0700

    Add a new Azure Linux 3 based crank-agent dockerfile and ability to use it. (#827)
    * Add a new Azure Linux 3 based crank-agent dockerfile and added basic support for specifying the dockerfile to use for the build script.
    Equivalents were not found for all packages. Specific ones not found include: cgroup-tools, software-properties-common, openssl-dbgsym, and libssl3t64-dbgsym. Others had what seem to be equivalents.

--- docker/agent/build.sh
+++ docker/agent/build.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 
 cpuname=$(uname -p)
-docker build -t crank-agent --build-arg CPUNAME=$cpuname -f Dockerfile ../../
+dockerfile="Dockerfile"
+
+while [ $# -ne 0 ]
+do
+    case "$1" in
+        --dockerfile)
+            shift
+            dockerfile="$1"
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            echo "Usage: $0 [--dockerfile <path>]"
+            exit 1
+            ;;
+    esac
+done
+
+docker build -t crank-agent --build-arg CPUNAME=$cpuname -f "$dockerfile" ../../
